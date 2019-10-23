2425e56a94683214394e3d6a3fa6e3b5f8913733
MVishukumar <beingvishu@gmail.com>
2019-10-23 15:34:16 +0530

    Added project lombok.

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package guru.springframework.services;
 
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Component
 public class RecipeServiceImpl implements RecipeService {
 
@@ -20,11 +22,9 @@ public class RecipeServiceImpl implements RecipeService {
     public Set<Recipe> getAllRecipes() {
         //return (Set<Recipe>) recipeRepository.findAll();
         Set<Recipe> recipes = new HashSet<>();
-
-
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
-
         System.out.println("recipes.size()" + recipes.size());
+        log.debug("recipes.size()" + recipes.size());
         return recipes;
 
     }
